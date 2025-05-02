7c6e1812c5f22f232157f889d6f43e3635fab160
seima-osako <osako.seima.su@alumni.tsukuba.ac.jp>
2025-05-02 15:35:44 +0200

    Add target group and ALB

--- terraform/modules/compute/outputs.tf
+++ terraform/modules/compute/outputs.tf
@@ -1,9 +1,9 @@
 output "ec2_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.app.id
+  value       = aws_instance.app_server.id
 }
 
 output "ec2_public_ip" {
   description = "Public IPv4 address of the EC2 instance"
-  value       = aws_instance.app.public_ip
+  value       = aws_instance.app_server.public_ip
 }
