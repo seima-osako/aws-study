3d7252a9d3bcb6676b53f081baf49a24ed06fb51
seima-osako <osako.seima.su@alumni.tsukuba.ac.jp>
2025-05-07 20:03:20 +0200

    fix outputs.tf of both compute and network

--- terraform/modules/compute/outputs.tf
+++ terraform/modules/compute/outputs.tf
@@ -7,3 +7,8 @@ output "ec2_public_ip" {
   description = "Public IPv4 address of the EC2 instance"
   value       = aws_instance.app.public_ip
 }
+
+output "alb_arn" {
+  description = "ARN of the Application Load Balancer"
+  value       = aws_lb.app.arn
+}
