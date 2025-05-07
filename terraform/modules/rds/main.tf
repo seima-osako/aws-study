4d86c4da6f636cdb955caa6da5b44ee41c15854a
seima-osako <osako.seima.su@alumni.tsukuba.ac.jp>
2025-05-07 20:01:36 +0200

    rds module

---
+++ terraform/modules/rds/main.tf
@@ -0,0 +1,38 @@
+############################
+# DB Subnet Group
+############################
+resource "aws_db_subnet_group" "main" {
+  name        = "${var.prefix}-rds-subnet-group"
+  description = "RDS private subnets (3AZ)"
+  subnet_ids  = var.db_subnet_ids
+
+  tags = {
+    Name = "${var.prefix}-rds-subnet-group"
+  }
+}
+
+############################
+# RDS Instance
+############################
+resource "aws_db_instance" "mysql" {
+  identifier                = "${var.prefix}-mysql"
+  engine                    = "mysql"
+  engine_version            = "8.0.39"
+  instance_class            = "db.t4g.micro"
+  allocated_storage         = 20
+  storage_type              = "gp2"
+  db_subnet_group_name      = aws_db_subnet_group.main.name
+  vpc_security_group_ids    = [var.rds_sg_id]
+  username                  = var.db_username
+  password                  = var.db_password
+  backup_retention_period   = 1
+  multi_az                  = false
+  publicly_accessible       = false
+  deletion_protection       = false
+  skip_final_snapshot       = false
+  final_snapshot_identifier = "${var.prefix}-mysql-final"
+
+  tags = {
+    Name = "${var.prefix}-mysql"
+  }
+}
---
+++ terraform/modules/rds/variables.tf
@@ -0,0 +1,35 @@
+variable "prefix" {
+  description = "Resource name prefix"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+}
+
+variable "db_subnet_ids" {
+  description = "List of subnet IDs for RDS"
+  type        = list(string)
+}
+
+variable "ec2_sg_id" {
+  description = "Security Group ID to attach to the RDS instance"
+  type        = string
+}
+
+variable "db_username" {
+  description = "Master username for the RDS instance"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Master password for the RDS instance"
+  type        = string
+}
+
+
+variable "rds_sg_id" {
+  description = "Security Group ID to attach to the RDS instance"
+  type        = string
+}
