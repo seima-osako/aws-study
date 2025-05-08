78135fa422f35f70c825ef7dc02c94d8e3481513
seima-osako <osako.seima.su@alumni.tsukuba.ac.jp>
2025-05-08 20:27:54 +0200

    fix fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,13 +15,11 @@ module "compute" {
   prefix            = var.prefix
   vpc_id            = module.network.vpc_id
   public_subnet_ids = module.network.public_subnet_ids
-
-  ami_id        = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-
-  alb_sg_id = module.security.alb_sg_id
-  ec2_sg_id = module.security.ec2_sg_id
+  ami_id            = var.ami_id
+  instance_type     = var.instance_type
+  key_name          = var.key_name
+  alb_sg_id         = module.security.alb_sg_id
+  ec2_sg_id         = module.security.ec2_sg_id
 }
 
 module "rds" {
